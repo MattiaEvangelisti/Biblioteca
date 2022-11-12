8ff47137676a400d71c8c0e75fad17b5de513c03
Eva <115864707+MattiaEvangelisti@users.noreply.github.com>
2022-11-12 09:53:02 +0100

    Add files via upload

---
+++ Biblioteca/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Biblioteca
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        libro newLibro;
+        string autore, titolo, editore;
+        int anno, numPag;
+
+
+        private void btnAddLibro_Click(object sender, RoutedEventArgs e)
+        {
+            autore = txtAutore.Text;
+            titolo = txtTitolo.Text;
+            editore = txtEditore.Text;
+            anno = int.Parse(txtAnno.Text);
+            numPag = int.Parse(txtAnno.Text);
+
+            libro newLibro = new libro(autore, titolo, editore, anno, numPag);
+        }
+        #region mostra dati
+        private void btnStampaTitolo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(newLibro.ShowTitolo(titolo));
+        }
+
+        private void btnStampaEditore_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(newLibro.ShowEditore(editore));
+        }
+
+
+        private void btnStampaNumPag_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(newLibro.ShowNumPag(numPag));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Non avevo tempo di far la parte della biblioteca");
+        }
+
+        private void btnStampaAnno_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(newLibro.ShowAnno(anno));
+        }
+
+        private void btnStampaAutore_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(newLibro.ShowAutore(autore));
+        }
+#endregion
+
+        private void btnModifica_Click(object sender, RoutedEventArgs e)
+        {
+            string autoreMod = txtAutore.Text, titoloMod = txtTitolo.Text, editoreMod = txtEditore.Text;
+
+            if (autoreMod.ToLower() == "nil")
+            {
+                //NIENTE
+            }
+            else
+            {
+                MessageBox.Show(newLibro.EditAutore(autoreMod));
+            }
+
+            if (titoloMod.ToLower() == "nil")
+            {
+                //NIENTE
+            }
+            else
+            {
+                MessageBox.Show(newLibro.EditTitolo(titoloMod));
+            }
+
+            if (editoreMod.ToLower() == "nil")
+            {
+                //NIENTE
+            }
+            else
+            {
+                MessageBox.Show(newLibro.EditEditore(editoreMod));
+            }
+
+            try
+            {
+                int annoMod = int.Parse(txtAnno.Text), numPagMod = int.Parse(txtNumPag.Text);
+                MessageBox.Show(newLibro.EditAnno(annoMod));
+                MessageBox.Show(newLibro.EditNumPag(numPagMod));
+            }
+            catch
+            {
+
+            }
+           
+        }
+    }
+}
---
+++ Biblioteca/libro.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal class libro
+    {
+        string autore, titolo, editore;
+        int anno, numPag;
+
+        int i = 0;
+        List<string> listaLibri;
+
+        public libro(string autore, string titolo, string editore, int anno, int numPag)
+        {
+            this.autore = autore;
+            this.titolo = titolo;
+            this.editore = editore;
+            this.anno = anno;
+            this.numPag = numPag;
+            i++;
+            listaLibri.Add(autore + "|" + titolo + "|" + editore + "|" + anno + "|" + numPag);
+        }
+
+        public string SearchTitolo(string titolo)
+        {
+            return titolo;
+        }
+
+        public string SearchLibri(string autore)
+        {
+            return autore;
+        }
+
+        public string NumLibri()
+        {
+            return ("Ci sono " + i.ToString() + " libro nella biblioteca");
+        }
+
+        public string toString()
+        {
+            return ("Autore: " + autore + "\nTitolo: " + titolo + "\nEditore: " + editore + "\nAnno di pubblicazione:" + anno + "\nNumero di pagine: " + numPag);
+        }
+        public void readingTime()
+        {
+            if (numPag < 100)
+            {
+                System.Windows.MessageBox.Show("Tempo di lettura: 1h");
+            }
+            else if (numPag > 100 && numPag < 200)
+            {
+                System.Windows.MessageBox.Show("Tempo di lettura: 2h");
+            }
+            else if (numPag > 200)
+            {
+                System.Windows.MessageBox.Show("Tempo di lettura: 2h+");
+            }
+        }
+        //DISPLAY DEI DATI
+        #region dati
+        public void showTitolo()
+        {
+            System.Windows.MessageBox.Show(titolo.ToString());
+        }
+        public void showAutore()
+        {
+            System.Windows.MessageBox.Show(autore.ToString());
+        }
+        public void showEditore()
+        {
+            System.Windows.MessageBox.Show(editore.ToString());
+        }
+        public void showAnno()
+        {
+            System.Windows.MessageBox.Show(anno.ToString());
+        }
+        public void showNumPag()
+        {
+            System.Windows.MessageBox.Show(numPag.ToString());
+        }
+        //MODIFICA DEI DATI
+
+        public void editTitolo(string titolo)
+        {
+            this.titolo = titolo;
+            System.Windows.MessageBox.Show("Dato Modificato con successo.");
+        }
+        public void editAutore(string autore)
+        {
+            this.autore = autore;
+            System.Windows.MessageBox.Show("Dato Modificato con successo.");
+        }
+        public void editEditore(string editore)
+        {
+            this.editore = editore;
+            System.Windows.MessageBox.Show("Dato Modificato con successo.");
+        }
+        public void editAnno(int anno)
+        {
+            this.anno = anno;
+            System.Windows.MessageBox.Show("Dato Modificato con successo.");
+        }
+        public void editNumPag(int numPag)
+        {
+            this.numPag = numPag;
+            System.Windows.MessageBox.Show("Dato Modificato con successo.");
+        }
+        #endregion
+    }
+}
